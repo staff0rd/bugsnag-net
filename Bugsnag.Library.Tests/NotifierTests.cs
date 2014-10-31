cf381eacb21eb75dc1e4f94f55da988eaf197d8f
staff0rd <staff0rd@users.noreply.github.com>
2014-10-31 16:40:33 +1100

    added empty exception test

--- Bugsnag.Library.Tests/NotifierTests.cs
+++ Bugsnag.Library.Tests/NotifierTests.cs
@@ -3,35 +3,51 @@ using Bugsnag.Library.Data;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Exception = System.Exception;
+
 namespace Bugsnag.Library.Tests
 {
     [TestClass]
     public class NotifierTests
     {
         [TestMethod]
-        public void TestAutoNotifier()
+        public void ApplicationExceptionTest()
         {
-
             try
             {
                 throw new ApplicationException("Throwing an app extension.  You heartless bastard.");
             }
-            catch(System.Exception ex)
+            catch(Exception e)
             {
-                BugSnag bs = new BugSnag
-                {
-                    ApiKey = "YOUR_API_KEY"
-                };
-
-                bs.Notify(ex, new
-                {
-                    OtherReallyCoolData = new
-                    {
-                        color = "Yellow",
-                        mood = "Mellow"
-                    }
-                });
+                Notify(e);
             }
+        }
+
+        [TestMethod]
+        public void BlankExceptionTest()
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception e)
+            {
+                Notify(e);
+            }
+        }
+
+        private static void Notify(Exception ex)
+        {
+            BugSnag bs = new BugSnag();
+
+            bs.Notify(ex, new
+                              {
+                                  OtherReallyCoolData = new
+                                                            {
+                                                                color = "Yellow",
+                                                                mood = "Mellow"
+                                                            }
+                              });
         }
     }
 }
